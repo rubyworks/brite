7070d09c3040c9d6b92174d240c05dd796f297ae
7rans <transfire@gmail.com>
2011-12-03 13:44:32 -0500

    Rewrite and simplify the rack server.

--- lib/brite/server.rb
+++ lib/brite/server.rb
@@ -1,112 +1,67 @@
 require 'fileutils'
+require 'tmpdir'
 require 'optparse'
+
+require 'rack'
 require 'rack/server'
 require 'rack/handler'
 require 'rack/builder'
 require 'rack/directory'
 require 'rack/file'
 
+require 'brite/config'
+
 module Brite
 
   # Brite::Server is a Rack-based server useful for testing sites locally.
-  # not all sites --in fact, most sites, can not be fully previewed via
-  # static files. A webserver is required to render and navigate a site
-  # completely. So this light server is provided to facilitate this.
-  class Server < ::Rack::Server
+  # Most sites cannot be fully previewed by loading static files into a browser.
+  # Rather, a webserver is required to render and navigate a site completely. 
+  # So this light server is provided to facilitate this.
+  #
+  class Server 
 
-    class Options
-      def parse!(args)
-        args, options = args.dup, {}
-
-        opt_parser = OptionParser.new do |opts|
-          opts.banner = "Usage: brite-server [options]"
-
-          opts.on("-p", "--port=port", Integer,
-                  "Runs server on the specified port.", "Default: 3000") { |v| options[:Port] = v }
-
-          opts.on("-b", "--binding=ip", String,
-                  "Binds server to the specified ip.", "Default: 0.0.0.0") { |v| options[:Host] = v }
-
-          opts.on("-c", "--config=file", String,
-                  "Use custom rackup configuration file.") { |v| options[:config] = v }
-
-          opts.on("-d", "--daemon", "Make server run as a Daemon.") { options[:daemonize] = true }
-
-          opts.on("-u", "--debugger", "Enable ruby-debugging for the server.") { options[:debugger] = true }
-
-          opts.on("-e", "--environment=name", String,
-                  "Specifies the environment to run this server under (test/development/production).",
-                  "Default: development") { |v| options[:environment] = v }
-
-          opts.separator ""
-
-          opts.on_tail("-h", "--help", "Show this help message.") { puts opts; exit }
-        end
-
-        opt_parser.parse!(args)
-
-        options[:server] = args.shift
-
-        return options
-      end
+    #
+    def self.start(argv)
+      new(argv).start
     end
 
     #
-    def initialize(*)
-      super
-      set_environment
+    def initialize(argv)
+      @options = ::Rack::Server::Options.new.parse!(argv)
+
+      @options[:app] = app
+      @options[:pid] = "#{tmp_dir}/pids/server.pid"
+
+      @options[:Port] ||= '3000'
     end
 
-    #
-    def opt_parser
-      Options.new
+    # THINK: Should we be using a local tmp directory instead?
+
+    # Temporary directory used by the rack server.
+    def tmp_dir
+      @tmp_dir ||= File.join(Dir.tmpdir, 'brite', root)
     end
 
-    #
-    def set_environment
-      ENV["BRITE_ENV"] ||= options[:environment]
-    end
-
-    #
     def start
-      puts "=> Booting #{server}"
-      puts "=> On http://#{options[:Host]}:#{options[:Port]}"
-      puts "=> Call with -d to detach" unless options[:daemonize]
-      trap(:INT) { exit }
-      puts "=> Ctrl-C to shutdown server" unless options[:daemonize]
+      ensure_brite_site
 
-      #Create required tmp directories if not found
+      # create required tmp directories if not found
       %w(cache pids sessions sockets).each do |dir_to_make|
-        FileUtils.mkdir_p(File.join('.cache', dir_to_make))
+        FileUtils.mkdir_p(File.join(tmp_dir, dir_to_make))
       end
 
-      super
-    ensure
-      # The '-h' option calls exit before @options is set.
-      # If we call 'options' with it unset, we get double help banners.
-      puts 'Exiting' unless @options && options[:daemonize]
+      ::Rack::Server.start(@options)
     end
 
-    def middleware
-      middlewares = []
-      #middlewares << [Rails::Rack::LogTailer, log_path] unless options[:daemonize]
-      #middlewares << [Rails::Rack::Debugger]  if options[:debugger]
-      Hash.new(middlewares)
+    def ensure_brite_site
+      return true if File.exist?(rack_file)
+      return true if config.file
+      abort "Not a brite site."
     end
 
-    def log_path
-      ".cache/brite.log"
-    end
-
-    def default_options
-      super.merge({
-        :Port        => 3000,
-        :environment => (ENV['BRITE_ENV'] || "development").dup,
-        :daemonize   => false,
-        :debugger    => false,
-        :pid         => ".cache/pids/server.pid",
-        :config_file => nil
-      })
+    #
+    def config
+      @config ||= Brite::Config.new(root)
     end
 
     #
@@ -114,11 +69,16 @@ module Brite
       Dir.pwd
     end
 
+    #
+    def rack_file
+     'brite.ru'
+    end
+
+    #
     def app
-      @app ||= begin
-        config_file = options[:config_file]
-        if config_file && ::File.exist?(config_file)
-          app, options = Rack::Builder.parse_file(config_file, opt_parser)
+      @app ||= (
+        if ::File.exist?(rack_file)
+          app, options = Rack::Builder.parse_file(rack_file, opt_parser)
           self.options.merge!(options)
           app
         else
@@ -129,16 +89,22 @@ module Brite
           #self.options.merge!(options)
           app
         end
-      end
+      )
     end
+
+    #
+    #def log_path
+    #  "_brite.log"
+    #end
+
+    #
+    #def middleware
+    #  middlewares = []
+    #  #middlewares << [Rails::Rack::LogTailer, log_path] unless options[:daemonize]
+    #  #middlewares << [Rails::Rack::Debugger]  if options[:debugger]
+    #  Hash.new(middlewares)
+    #end
 
   end
 
 end
-
-## The static content rooted in the current working directory
-## Eg. Dir.pwd =&gt; http://0.0.0.0:3000/
-#root = Dir.pwd
-#puts ">>> Serving: #{root}"
-#run Rack::Directory.new("#{root}")
-
