41b4db00ca945097578c6b5a40b95b5f63ac7adb
7rans <transfire@gmail.com>
2011-12-03 13:46:54 -0500

    Provide project metadata via .ruby file.

--- lib/brite/version.rb
+++ lib/brite/version.rb
@@ -1,3 +1,21 @@
 module Brite
-  VERSION = '0.6.0'
+
+  # Access to project metadata as given in `brite.yaml` which
+  # is soft linked to the `.ruby` project file.
+  #
+  # @return [Hash] project metadata
+  def self.metadata
+    @metadata ||= (
+      require 'yaml'
+      YAML.load(File.dirname(__FILE__) + '/../brite.yml')
+    )
+  end
+
+  # If constant is missing, check project metadata.
+  #
+  # @raise [NameError] uninitialized constant
+  def self.const_missing(name)
+    metadata[name.to_s.downcase] || super(name)
+  end
+
 end
