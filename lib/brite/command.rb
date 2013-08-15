0acac9f479c3b8aac171da033906246f9600758e
7rans <transfire@gmail.com>
2013-08-14 20:03:34 -0400

    Fix help command. [bug]

--- lib/brite/command.rb
+++ lib/brite/command.rb
@@ -117,7 +117,7 @@ module Brite
     # @private
     def options_help(parser, options)
       parser.on_tail("--help", "display this help message") do
-        puts opt
+        puts options
         exit
       end
     end
